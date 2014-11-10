f1f78cb7eabf013a6df5596e78c4e11bf4f54ddc
Rob Taylor <rob@uxblondon.com>
2014-11-10 12:27:50 +0000

    Refactored sitemap generation with Boom\Sitemap class

--- classes/Controller/Sitemap.php
+++ classes/Controller/Sitemap.php
@@ -9,20 +9,6 @@ class Controller_Sitemap extends Boom\Controller
 		// Set an content-type header.
 		$this->response
 			->headers('Content-Type', 'text/xml')
-			->body(
-				View::factory('boom/sitemap/xml', array(
-					'base_url'	=>	URL::base($this->request),
-					'pages'	=>	$this->getPages(),
-				))
-			);
-	}
-
-
-	protected function getPages()
-	{
-		$finder = new Page\Finder;
-        $finder->addFilter(new Page\Finder\Filter\VisibleToSearchEngines());
-
-        return $finder->findAll();
+			->body(new Boom\Sitemap());
 	}
 }
